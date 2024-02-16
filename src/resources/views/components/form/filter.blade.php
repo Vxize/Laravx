24453fa48e49f319ce76d2fdcc164cf66b3f4aaf
NoDesireMonk <nodesiremonk@gmail.com>
2024-02-15 22:19:36 -0600

    move reset button after filter button in filter template

--- src/resources/views/components/form/filter.blade.php
+++ src/resources/views/components/form/filter.blade.php
@@ -15,22 +15,24 @@
     <input type="hidden" name="search" value="{{ $query['search'] }}" >
 @endif
     {{ $slot }}
-    <x-lavx::button
-        display="inline-block"
-        color="red"
-        link="{{ route($route, $search) }}"
-        icon="fa6-solid:filter-circle-xmark"
-        text="{{ __('lavx::sys.reset') }}"
-        textSize="lg:text-lg md:text-md text-base"
-        padding="p-2"
-        width="w-28"
-        margin="mx-auto"
-    />
     <x-slot:submit>
         <x-lavx::form.submit
             display="inline-block"
             icon="fa6-solid:filter"
             text="{{ __('lavx::sys.filter') }}"
+            textSize="lg:text-lg md:text-md text-base"
+            padding="p-2"
+            width="w-28"
+            margin="mx-auto"
+        />
+    </x-slot>
+    <x-slot:after_submit>
+        <x-lavx::button
+            display="inline-block"
+            color="red"
+            link="{{ route($route, $search) }}"
+            icon="fa6-solid:filter-circle-xmark"
+            text="{{ __('lavx::sys.reset') }}"
             textSize="lg:text-lg md:text-md text-base"
             padding="p-2"
             width="w-28"
